2be0089ed1962516141d7b3243f1331f42605bce
Taylor <taylort3450@proton.me>
2026-06-20 02:39:21 -0500

    chore: cleanup old comments

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,3 @@ fn main() {
         user_input.clear();
     }
 }
-
-/*
- * In this stage, you'll implement support for printing error messages for invalid commands.
- *
- *  Example:
- *  $ xyz
- *  xyz: command not found
- *
- *  Your program should:
- *
- *  Display the prompt $ (keep the code from the previous stage)
- *  Read the user's input
- *  Print an error message in exactly this format: {command}: command not found
- *      e.g. if the user types xyz, print xyz: command not found
- *
- *  For now, we'll treat all commands as "invalid". In later stages we'll handle executing "valid" commands like echo, cd etc.
- *
- */
