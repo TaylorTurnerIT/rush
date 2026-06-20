97bfcb9c4e2e1cbef161693203ea0f5540bd8700
Taylor <taylort3450@proton.me>
2026-06-20 02:52:37 -0500

    Stage Five: Add exit command handling

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,26 @@ fn main() {
     let stdin = io::stdin();
 
     loop {
+        // Shell prefix output
         print!("$ ");
         io::stdout().flush().unwrap();
 
+        // Get user input
         stdin
             .read_line(&mut user_input)
             .expect("Failed to read line");
 
+        // Input handling
         let trimmed_user_input = user_input.trim();
-        if !trimmed_user_input.is_empty() {
-            println!("{}: command not found", trimmed_user_input);
+
+        if trimmed_user_input.is_empty() {
+            user_input.clear();
+            continue;
+        }
+
+        match trimmed_user_input {
+            "exit" => break,
+            _ => println!("{}: command not found", trimmed_user_input),
         }
 
         user_input.clear();
