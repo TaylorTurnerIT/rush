86a858e5543d7c94a7640691fd022f5ce3fab2d1
Taylor <taylort3450@proton.me>
2026-06-20 02:06:59 -0500

    Stage Two: added io but stdin buffer problem ongoing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Read, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+
+    loop {
+        print!("$ ");
+        stdin
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
+        println!("{} is not a recognised command", user_input.trim());
+        io::stdout().flush().unwrap();
+        break;
+    }
 }
+
+/*
+ * In this stage, you'll implement support for printing error messages for invalid commands.
+ *
+ *  Example:
+ *  $ xyz
+ *  xyz: command not found
+ *
+ *  Your program should:
+ *
+ *  Display the prompt $ (keep the code from the previous stage)
+ *  Read the user's input
+ *  Print an error message in exactly this format: {command}: command not found
+ *      e.g. if the user types xyz, print xyz: command not found
+ *
+ *  For now, we'll treat all commands as "invalid". In later stages we'll handle executing "valid" commands like echo, cd etc.
+ *
+ */
