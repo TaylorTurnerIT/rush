c41a5ed40807a15586ab77fbc30bfe5b425f006c
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2026-06-24 17:25:59 -0500

    feat: refactored command logic to use binaries over functions

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,17 @@
 #[allow(unused_imports)]
 use std::env;
+use std::process::Command;
 use std::{
     io::{self, Write},
-    os::unix::fs::PermissionsExt,
+    // os::unix::fs::PermissionsExt,
 };
 
-#[derive(PartialEq)]
-enum ControlFlow {
-    Break,
-    Continue,
-    TypeBuiltin,
-    TypeFile,
-    TypeNotFound,
-}
-
-fn exec_command(command: &str, args: &Vec<&str>, is_type: bool) -> ControlFlow {
-    match command {
-        // -------------------------------------------------------------------------
-        "exit" => {
-            if !is_type {
-                return ControlFlow::Break;
-            }
-        }
-        // -------------------------------------------------------------------------
-        "echo" => {
-            if !is_type {
-                println!("{}", args.join(" "))
-            }
-        }
-        // -------------------------------------------------------------------------
-        "type" => {
-            if args.is_empty() {
-                return ControlFlow::Continue;
-            }
-            if is_type {
-                return ControlFlow::TypeBuiltin;
-            }
-
-            if exec_command(args[0], &args, true) == ControlFlow::TypeNotFound {
-                // check PATH
-                let file_location = search_path(&args[0]);
-                // file found
-                if file_location != "" {
-                    if is_type {
-                        return ControlFlow::TypeFile;
-                    }
-                    println!("{} is {}", &args[0], file_location);
-                    return ControlFlow::Continue;
-                }
-                println!("{}: not found", args[0])
-            } else {
-                println!("{} is a shell builtin", args[0])
-            }
-        }
-        // -------------------------------------------------------------------------
-        _ => {
-            // not builtin AND not file
-            if is_type {
-                return ControlFlow::TypeNotFound;
-            }
-            println!("{}: command not found", command)
-        }
-    }
-
-    if is_type {
-        return ControlFlow::TypeBuiltin;
-    } else {
-        return ControlFlow::Continue;
-    }
-}
-
-fn search_path(command: &str) -> String {
-    // let command_path = env::var("PATH").unwrap();
-    // println!("{}", env::current_dir().unwrap().display());
-    // println!("{:#?}", env::var("PATH")); // this is how you get PATH
-
-    let key = "PATH";
-    match env::var_os(key) {
-        Some(paths) => {
-            for path in env::split_paths(&paths) {
-                let mut current_path_dir = match path.read_dir() {
-                    Ok(dir) => dir,
-                    _ => continue, // ignore missing directories in path
-                };
-
-                // println!(
-                //     "debug: {:#?}",
-                //     current_path_dir.find(|x| x.as_ref().unwrap().file_name() == command)
-                // );
-
-                let matched_file =
-                    current_path_dir.find(|x| x.as_ref().unwrap().file_name() == command);
-                match matched_file {
-                    Some(Ok(f)) => {
-                        // println!("{}", f.path().display()); //debug print
-                        // println!("{:o}", f.metadata().unwrap().permissions().mode());
-                        if f.metadata().unwrap().permissions().mode() & 0o111 != 0 {
-                            return f.path().display().to_string();
-                        }
-                        continue;
-                    }
-                    _ => continue,
-                }
-            }
-        }
-        None => println!("{key} is not defined in the environment."),
-    }
-    return "".to_string();
+fn exec_command(command: &str, args: &Vec<&str>) {
+    Command::new(command)
+        .args(args)
+        .output()
+        .expect("failed to execute process");
+    return;
 }
 
 fn main() {
@@ -116,38 +21,38 @@ fn main() {
     loop {
         // Shell prefix output
         print!("$ ");
-        io::stdout().flush().unwrap();
+        match io::stdout().flush() {
+            Ok(_t) => (),
+            Err(e) => println!("Err: {}", e),
+        }
 
         // Get user input
-        stdin
-            .read_line(&mut user_input)
-            .expect("Failed to read line");
+        match stdin.read_line(&mut user_input) {
+            Ok(_t) => (),
+            Err(e) => eprint!("Error: {}", e),
+        }
 
         // Input processing
         let trimmed_user_input = user_input.trim();
-
         if trimmed_user_input.is_empty() {
             user_input.clear();
             continue;
         }
 
         let mut tokens = trimmed_user_input.split_whitespace();
-        let command = tokens.next().unwrap();
+        let command;
+        match tokens.next() {
+            Some(t) => command = t,
+            None => continue,
+        }
         let args: Vec<&str> = tokens.collect();
 
         // Command handling
-        match exec_command(&command, &args, false) {
-            ControlFlow::Break => break,
-            ControlFlow::Continue => {
-                user_input.clear();
-                continue;
-            }
-            _ => {
-                panic!(
-                    "type command escaped with parameters: {}",
-                    trimmed_user_input
-                );
-            }
+        if command == "exit" {
+            break;
         }
+
+        exec_command(&command, &args);
+        user_input.clear();
     }
 }
