51c3cc6a42ec1b26ddcc6cdaecc61e804f2761a3
Taylor Turner <131205485+TaylorTurnerIT@users.noreply.github.com>
2026-06-27 21:53:02 -0500

    codecrafters submit [skip ci]

--- src/builtin/echo/main.rs
+++ src/builtin/echo/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    let message: Vec<String> = std::env::args().skip(1).collect();
+    let message: String = std::env::args().skip(1).collect();
     print!("{:#?}", message);
     return;
 }
